f38074d0ab6e2b6f4ba1637d9854bcd752344ed7
Yoann <yoann.menard@epitech.eu>
2021-01-21 18:15:59 +0100

    Création des méthodes shuffle(), insertAt(), draw(), getCardsCount()

--- src/models/deck.js
+++ src/models/deck.js
@@ -1,14 +1,27 @@
 
 export default class Deck {
     constructor (config) {
+        this.cards = config.cards;
     }
 
     shuffle () {
+        const unshuffled = [this.cards];
+        if(unshuffled.length > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    insertAt(card, position) {
+        
     }
 
     draw () {
+        return this.cards.shift();
     }
 
     getCardsCount () {
+        return this.cards.length;
     }
 }
